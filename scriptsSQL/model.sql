44e6d5059472380a1af6a889ba397954871a5dd9
alejandro <alejandrobravo0329@gmail.com>
2022-05-17 12:13:58 -0500

    added scripts for database

---
+++ scriptsSQL/model.sql
@@ -0,0 +1,77 @@
+DROP TABLE IF EXISTS public."ControllableDevice" CASCADE;
+DROP TABLE IF EXISTS public."IOModule" CASCADE;
+DROP TABLE IF EXISTS public."Channel" CASCADE;
+DROP TABLE IF EXISTS public."Value" CASCADE;
+DROP SEQUENCE IF EXISTS seq_controllable;
+DROP SEQUENCE IF EXISTS seq_IOModule;
+DROP SEQUENCE IF EXISTS seq_Channel;
+DROP SEQUENCE IF EXISTS seq_Value;
+
+
+
+CREATE TABLE public."ControllableDevice" (
+	id integer NOT NULL,
+	type varchar,
+	processor varchar,
+	CONSTRAINT "ControllableDevice_pk" PRIMARY KEY (id)
+);
+ALTER TABLE public."ControllableDevice" OWNER TO postgres;
+
+CREATE SEQUENCE seq_controllable
+INCREMENT 1
+START 1;
+
+CREATE TABLE public."IOModule" (
+	id integer NOT NULL,
+	name varchar,
+	type varchar,
+	"id_ControllableDevice" integer,
+	CONSTRAINT "IOModule_pk" PRIMARY KEY (id)
+);
+ALTER TABLE public."IOModule" OWNER TO postgres;
+
+CREATE SEQUENCE seq_IOModule
+INCREMENT 1
+START 1;
+
+ALTER TABLE public."IOModule" ADD CONSTRAINT "ControllableDevice_fk" FOREIGN KEY ("id_ControllableDevice")
+REFERENCES public."ControllableDevice" (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+CREATE TABLE public."Channel" (
+	id integer NOT NULL,
+	type varchar,
+	signal varchar,
+	range varchar,
+	unit varchar,
+	"id_IOModule" integer,
+	CONSTRAINT "Channel_pk" PRIMARY KEY (id)
+);
+ALTER TABLE public."Channel" OWNER TO postgres;
+
+CREATE SEQUENCE seq_Channel
+INCREMENT 1
+START 1;
+
+ALTER TABLE public."Channel" ADD CONSTRAINT "IOModule_fk" FOREIGN KEY ("id_IOModule")
+REFERENCES public."IOModule" (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+CREATE TABLE public."Value" (
+	id integer NOT NULL,
+	"timestamp" timestamp,
+	value double precision,
+	"id_Channel" integer,
+	CONSTRAINT value_pk PRIMARY KEY (id)
+);
+ALTER TABLE public."Value" OWNER TO postgres;
+
+CREATE SEQUENCE seq_Value
+INCREMENT 1
+START 1;
+
+ALTER TABLE public."Value" ADD CONSTRAINT "Channel_fk" FOREIGN KEY ("id_Channel")
+REFERENCES public."Channel" (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+
---
+++ scriptsSQL/population.sql
@@ -0,0 +1,17 @@
+INSERT INTO public."ControllableDevice" values (nextval('seq_controllable'),'GroovEpic', 'GRV-EPIC-PR2');
+INSERT INTO public."ControllableDevice" values (nextval('seq_controllable'),'RIO', 'opto22');
+
+INSERT INTO public."IOModule" values (nextval('seq_IOModule'),'GRV-OAC-12','Output',1);
+INSERT INTO public."IOModule" values (nextval('seq_IOModule'),'GRV-IACI-12 ','Input',1);
+
+INSERT INTO public."Channel" values (nextval('seq_Channel'),'Digital','Digital','0-20','temperature',1);
+INSERT INTO public."Channel" values (nextval('seq_Channel'),'Digital','Digital','0-20','temperature',1);
+INSERT INTO public."Channel" values (nextval('seq_Channel'),'Digital','Digital','0-20','temperature',1);
+INSERT INTO public."Channel" values (nextval('seq_Channel'),'Digital','Digital','0-20','temperature',1);
+INSERT INTO public."Channel" values (nextval('seq_Channel'),'Digital','Digital','0-20','temperature',1);
+INSERT INTO public."Channel" values (nextval('seq_Channel'),'Digital','Digital','0-20','temperature',1);
+INSERT INTO public."Channel" values (nextval('seq_Channel'),'Digital','Digital','0-20','temperature',1);
+INSERT INTO public."Channel" values (nextval('seq_Channel'),'Digital','Digital','0-20','temperature',1);
+
+INSERT INTO public."Value" values (nextval('seq_Value'),'2022-05-17 11:07:00',3.1,1);
+
