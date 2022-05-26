06fa08b8425ba2c1e5b99a954f84ac63d5b28d5c
Diego H <d13g01hl@gmail.com>
2022-05-26 18:05:59 -0500

    Added methods to create a new mqtt client

---
+++ Code/python/MQTT/src/model/Client.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as paho
+from paho import mqtt
+
+# setting callbacks for different events to see if it works, print the message etc.
+def on_connect(client, userdata, flags, rc, properties=None) :
+    print("CONNACK received with code %s." % rc)
+
+# with this callback you can see if your publish was successful
+def on_publish(client, userdata, mid, properties=None) :
+    print("mid: " + str(mid))
+
+# print which topic was subscribed to
+def on_subscribe(client, userdata, mid, granted_qos, properties=None) :
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg) :
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+# creates a new client with the specified username, password, host and port
+def create_client(user, password, host, port) :
+    client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+    client.username_pw_set(user, password)
+    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    client.on_connect = on_connect
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+    client.on_publish = on_publish
+    client.connect(host, port)
+    return client
+
+
+
+
