cb0bd1d20f9e67110ecd7ad7890994a3825929cc
Diego H <d13g01hl@gmail.com>
2022-05-26 21:51:20 -0500

    Implementing connection to database

--- Code/python/MQTT/src/model/Subscriber.py
+++ Code/python/MQTT/src/model/Subscriber.py
@@ -1,4 +1,5 @@
 import Client
+import psycopg2
 
 user = input("Username: ")
 password = input("Password: ")
@@ -12,5 +13,22 @@ while True :
     if(topic == "s") :
         break
     my_client.subscribe(topic, qos = 1)
+
+# WyksF8.Um58i9YH
+
+def establish_db_connection(db, u, p) :
+    return psycopg2.connect(database = db, user = u, password = p)
+    
+connection = establish_db_connection("Test", "postgres", "Diego-SQL")
+cursor1 = connection.cursor()
+
+def on_message(client, userdata, msg) :
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    sql = "INSERT INTO Test_Values(val) values(%s)"
+    data = (msg.payload.decode('UTF-8'),)
+    cursor1.execute(sql, data)
+    connection.commit()
+
+my_client.on_message = on_message
 
 my_client.loop_forever()
