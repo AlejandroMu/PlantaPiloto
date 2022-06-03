1d871ad10ce42185d368a939e4a5842a5da848d7
Diego H <d13g01hl@gmail.com>
2022-06-03 00:58:53 -0500

    Modified database connection

--- Code/python/MQTT/src/model/Subscriber.py
+++ Code/python/MQTT/src/model/Subscriber.py
@@ -2,6 +2,7 @@ from getpass import getpass
 import sys
 import Client
 import psycopg2
+import datetime
 
 if(len(sys.argv) != 3) :
     print('Please provide module # and channel # to suscribe.')
@@ -13,10 +14,18 @@ chN = str(sys.argv[2])
 
 topic = modN + '/' + chN
 
+print('---- BROKER INFORMATION ----')
 user = input("Username: ")
 password = getpass()
 host = input("Broker url: ")
 port = int(input("port: "))
+print('----------------------------\n')
+
+print('--- DATABASE INFORMATION ---')
+db_name = input("DataBase: ")
+db_user = input("Username: ")
+db_password = getpass()
+print('----------------------------\n')
 
 my_client = Client.create_client(user, password, host, port)
 
@@ -25,13 +34,17 @@ my_client.subscribe(topic, qos = 1)
 def establish_db_connection(db, u, p) :
     return psycopg2.connect(database = db, user = u, password = p)
     
-connection = establish_db_connection("Test", "postgres", "Diego-SQL")
+connection = establish_db_connection(db_name, db_user, db_password)
 cursor1 = connection.cursor()
 
 def on_message(client, userdata, msg) :
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-    sql = "INSERT INTO Test_Values(val) values(%s)"
-    data = (msg.payload.decode('UTF-8'),)
+    sql = 'INSERT INTO public."Value" values(nextval("seq_Value"), %s)'
+    e = datetime.datetime.now()
+    time_s = e.strftime("%Y-%m-%d %H:%M:%S")
+    value = float(msg.payload.decode('UTF-8'))
+    channel = int(str(msg.topic).split("/")[1])
+    data = (time_s, value, channel,)
     cursor1.execute(sql, data)
     connection.commit()
 
