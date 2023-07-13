579e977f70797572d56a7be1d62300f37396e9fd
alejandro <alejandrobravo0329@gmail.com>
2023-07-12 19:52:32 -0500

    db script

--- Code/scriptsSQL/install_db.sh
+++ Code/scriptsSQL/install_db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export PGPASSWORD="postgres"
-psql -U postgres -h xhgrid2 -c "ALTER DATABASE assets CONNECTION LIMIT 0;"
-psql -U postgres -h xhgrid2 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'assets' AND pid <> pg_backend_pid();"
-psql -U postgres -h xhgrid2 -v user=ingswi40 -v password=IngSwI40PWD -f user-db.sql
+host=$1
+psql -U postgres -h $host -c "ALTER DATABASE assets CONNECTION LIMIT 0;"
+psql -U postgres -h $host -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'assets' AND pid <> pg_backend_pid();"
+psql -U postgres -h $host -v user=ingswi40 -v password=IngSwI40PWD -f user-db.sql
 export PGPASSWORD="IngSwI40PWD"
-psql -U ingswi40 -h xhgrid2 -d assets -f model.sql
-psql -U ingswi40 -h xhgrid2 -d assets -f model_inserts.sql
+psql -U ingswi40 -h $host -d assets -f model.sql
+psql -U ingswi40 -h $host -d assets -f model_inserts.sql
